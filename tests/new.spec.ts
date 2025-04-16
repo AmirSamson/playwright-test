b4a5f0ae8ad794c518553201472268f8cb5c097a
Amir Samson <amirhsamson93@gmail.com>
2025-04-16 20:09:09 +0330

    pushing the new test files to the repo

---
+++ tests/new.spec.ts
@@ -0,0 +1,25 @@
+import {expect, test, chromium } from "@playwright/test";
+
+test.use({
+    baseURL: "https://fedshi.com"
+})
+
+test('should click on the button', async ({page}) => {
+    const browser = chromium.launch({
+        headless: false
+    })
+    await page.goto('/en/home/');
+    await expect(page.url()).toBe("https://www.fedshi.com/en/home/")
+    await expect(page).toHaveURL("https://www.fedshi.com/en/home/")
+
+    await page.click('"Home"')
+    await page.click('"Suppliers"')
+    await page
+    .getByRole('link', {name: 'ويبستور فدشي'})
+    // .locator('a[href=https://web.fedshi.com/auth/login', {hasText: 'ويبستور فدشي'})
+    .click();
+    await page.waitForTimeout(2000)
+    await page
+    .locator('.heading')
+    .screenshot({ path: './test-results/screenshots/screenshot.png' });
+})
