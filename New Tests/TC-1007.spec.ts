fb83c51669db1350336f85f8d88f7c8f6aff8e7a
Amir Samson <amirhsamson93@gmail.com>
2025-04-21 20:16:16 +0330

    added new TC-1007

--- New Tests/TC-1007.spec.ts
+++ New Tests/TC-1007.spec.ts
@@ -29,7 +29,6 @@ test('click on Continue button to setting password', async ({page}) => {
     await textbox.fill('12345678');
     await buttonLoc.filter({ hasText: 'تسجيل الدخول' }).click();
     await page.waitForURL("https://webstore.demo.fedshi.ice.global/checkout/cart")
-    await expect(page).toHaveURL("https://webstore.demo.fedshi.ice.global/checkout/cart")
     await page.waitForTimeout(2000)
     
     //from here:
