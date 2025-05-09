71e301d5b2d13d43e36734c27076aba76d050622
Amir Samson <amirhsamson93@gmail.com>
2025-05-09 23:59:37 +0330

    Reorganized folders

--- Old tests/temp/github test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-
-test('github issue writter prototype', async ({ page }) => {
-
-    await page.goto('https:/github.com')
-    await page.click("text=sign in");
-    await page
-
-})
--- Old tests/test-101.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('test', async ({ page }) => {
-    
- await page.goto('https://mail.google.com/');
-//  await page.locator('text=ورود').click();
- });
