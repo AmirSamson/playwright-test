370b55cfdda54df7d674e982659ce93594397a4d
Amir Samson <amirhsamson93@gmail.com>
2025-06-21 16:05:43 +0330

    added another assertion and not.to.expect

--- tests/Digikala.spec.ts
+++ tests/Digikala.spec.ts
@@ -20,6 +20,25 @@ test.describe('check values and consts', () => {
 
         await expect(page.getByRole('link').filter({ hasText: array[0] }).first()).toContainText(array[0])
     }) 
+
+        test('expect and not to expect', async ({page}) => {
+        await page.goto('https://www.digikala.com/')
+        const SearchBar = await page.getByText('جستجو');
+
+        await SearchBar.click()
+        await page.locator('input[name="search-input"]')
+        .fill(array[0]);
+        await SearchBar.press('Enter');
+
+        await page.locator('input[name="search-input"]')
+        .fill(array[1])
+        await SearchBar.press('Enter');
+
+            const FirstLink = await page.getByRole('link').filter({hasText:array[1]}).first();
+        await expect(FirstLink).toContainText(array[1])
+        await expect(FirstLink).not.toContainText(array[2]);
+
+    }) 
 });
 
 
