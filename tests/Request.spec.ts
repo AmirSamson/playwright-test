e8d0ecc17306a9ae062246b560e033bee90fa595
Amir Samson <amirhsamson93@gmail.com>
2025-06-22 22:48:53 +0330

    added the API call for the cookies

---
+++ tests/Request.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+import { request } from "http";
+
+test('requesting an API', async ({page, request, context})=>{
+    const resPonse = await request.post('https://accounts.mail.ir/app/sign-up', {
+        data:{
+            identifier: "zjlyd@telegmail.com", 
+            username: "zjlyd", 
+            first_name: "اساسا", 
+            last_name: "اساسا",
+            display_name: "اساسا",
+            newPassword: "UXdlcjEyMzRA"
+        },
+    });
+    const responseBody = await resPonse.json();
+    const access_Token = responseBody.access_Token;
+    const first_name = responseBody.first_name;
+    const userID = responseBody.userID
+
+    console.log(access_Token, first_name, userID);
+
+    // this is an array of cookies. 
+    // every cookie has different properties.
+
+    await context.addCookies([
+        {
+            url: 'https://accounts.mail.ir',
+            name: 'access_Token',
+            value: 'access_Token'
+        },
+        {
+            url: 'https://accounts.mail.ir',
+            name: 'first_name',
+            value: 'first_name'
+        },
+        {
+            url: 'https://accounts.mail.ir',
+            name: 'userID',
+            value: 'userID'
+        },
+    ]);
+})
