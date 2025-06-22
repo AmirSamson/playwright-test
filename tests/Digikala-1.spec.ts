0fb26a2e9f42277bdd5b4eb880071e2aa81edfe6
Amir Samson <amirhsamson93@gmail.com>
2025-06-22 16:38:15 +0330

    added the baseURL in all files.

--- tests/Digikala-1.spec.ts
+++ tests/Digikala-1.spec.ts
@@ -1,33 +1,37 @@
 import {test, expect} from '@playwright/test'
 
+test.use({
+    baseURL: "https://www.digikala.com"
+})
+
 const array = ['تبلت', 'گوشی', 'Xiaomi' ] as const;
 
 test('expect visibilty on search box', async ({page}) => {
-    await page.goto('https://www.digikala.com/')
+    await page.goto('')
     const SearchBar = await page.getByText("جستجو");
     await expect(SearchBar).toBeVisible();
 });
 
 
 test('checkboxing Radio button, expecting URL change', async ({page}) => {
-        await page.goto('https://www.digikala.com/')
-        await page.waitForURL('https://www.digikala.com/')
+        await page.goto('')
+        await page.waitForURL('')
         const SearchBar = await page.getByText('جستجو');
 
         await SearchBar.click()
         await page.locator('input[name="search-input"]')
         .fill(array[0]);
         await SearchBar.press('Enter');
-        await expect(page).toHaveURL('https://www.digikala.com/search/?q=تبلت')
+        await expect(page).toHaveURL('/search/?q=تبلت')
 
         await page.waitForTimeout(1500)
         await page.locator('[class="block w-full relative h-full styles_switchInput__track__ftJN8 styles_switchInput__track--border--400__474Fy"]').nth(1).click();
-        await expect(page).toHaveURL('https://www.digikala.com/search/?has_ship_by_seller=1&q=تبلت')
+        await expect(page).toHaveURL('/search/?has_ship_by_seller=1&q=تبلت')
 });
 
 test('forLoop -> consistent', async ({page})=>{
-    await page.goto('https://www.digikala.com/')
-    await page.waitForURL('https://www.digikala.com/')
+    await page.goto('')
+    await page.waitForURL('')
     const SearchBar = await page.getByText('جستجو');
 
     for (const Portion of array.slice(0, 2)) {
@@ -39,3 +43,10 @@ test('forLoop -> consistent', async ({page})=>{
 
     }
 })
+
+
+test('Defining inside Functions', async ({page})=>{
+    await page.goto('')
+    await page.waitForURL('')
+    await page.dblclick('[class="lg:text-body-2 text-button-1 flex items-center h-full text-body-2 text-neutral-500"]');
+});
