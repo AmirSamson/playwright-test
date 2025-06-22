52439e5e02b85d5ea0c055bec17b7a252831b6ed
Amir Samson <amirhsamson93@gmail.com>
2025-06-22 16:40:01 +0330

    added the baseURL in all files.

--- tests/Digikala.spec.ts
+++ tests/Digikala.spec.ts
@@ -5,12 +5,16 @@ import { link } from 'fs';
 // await page.goto('https://www.digikala.com/')
 // });
 
+test.use({
+    baseURL: 'https://www.digikala.com'
+})
+
 const array = ['تبلت', 'گوشی', 'Xiaomi' ] as const;
 
 test.describe('check values and consts', () => {
     
     test('add strings from array to search box', async ({page}) => {
-        await page.goto('https://www.digikala.com/')
+        await page.goto('')
         const SearchBar = await page.getByText('جستجو');
 
         await SearchBar.click()
@@ -22,15 +26,15 @@ test.describe('check values and consts', () => {
     });
 
         test('expect and not to expect', async ({page}) => {
-        await page.goto('https://www.digikala.com/')
-        await page.waitForURL('https://www.digikala.com/')
+        await page.goto('')
+        await page.waitForURL('')
         const SearchBar = await page.getByText('جستجو');
 
         await SearchBar.click()
         await page.locator('input[name="search-input"]')
         .fill(array[0]);
         await SearchBar.press('Enter');
-        await expect(page).toHaveURL('https://www.digikala.com/search/?q=تبلت')
+        await expect(page).toHaveURL('/search/?q=تبلت')
 
         await page.waitForTimeout(3000)
         await page.locator('[class="lg:text-body-2 text-button-1 flex items-center h-full text-body-2 text-neutral-800"]').click()
@@ -44,15 +48,15 @@ test.describe('check values and consts', () => {
     }) ;
 
         test('clearing and expecting to be Empty', async ({page}) => {
-        await page.goto('https://www.digikala.com/')
-        await page.waitForURL('https://www.digikala.com/')
+        await page.goto('')
+        await page.waitForURL('')
         const SearchBar = await page.getByText('جستجو');
 
         await SearchBar.click()
         await page.locator('input[name="search-input"]')
         .fill(array[0]);
         await SearchBar.press('Enter');
-        await expect(page).toHaveURL('https://www.digikala.com/search/?q=تبلت')
+        await expect(page).toHaveURL('/search/?q=تبلت')
 
         await page.waitForTimeout(2000)
         await page.locator('[class="lg:text-body-2 text-button-1 flex items-center h-full text-body-2 text-neutral-800"]')
