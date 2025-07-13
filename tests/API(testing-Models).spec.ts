523f2679480352148712a65d975e0af1f22b7dc6
Amir Samson <amirhsamson93@gmail.com>
2025-07-13 16:30:25 +0330

    added the APIs and Test for adding a Todo item.

--- tests/API(testing-Models).spec.ts
+++ tests/API(testing-Models).spec.ts
@@ -1,8 +1,6 @@
 import test from "@playwright/test";
 import testingUser from "../Models/userTest";
 import UserAPIsTest from "../APIs/UserAPIs(test)";
-import userTest from "../Models/userTest";
-
 
 // test.use({
 //     baseURL: 'http://todo.qacart.com/'
@@ -42,3 +40,51 @@ test('Register the user - removing API and adding the Models instead.', async({r
 
     console.log(access_token, firstName, userID)    
 });
+
+
+
+
+test('Adding a Todo item - using models.', async ({page, request, context})=>{
+
+    const NewUser = new testingUser(
+        'hey2', 
+        'hey3', 
+        "520@gmail.com",
+        '1234qwer@A',
+    );
+
+    const loginRes = await new UserAPIsTest().Login(request, NewUser);
+
+
+    const responseBodys = await loginRes.json();
+    const access_token = responseBodys.access_token;
+    const firstName = responseBodys.firstName;
+    const userID = responseBodys.userID;
+
+    await context.addCookies([
+        {   name: 'access_token', 
+            value:access_token, 
+            url: 'https://todo.qacart.com/'
+        },
+        {   name:'firstName', 
+            value:firstName, 
+            url: 'https://todo.qacart.com/' 
+        },
+        {   name:'userID', 
+            value:userID, 
+            url: 'https://todo.qacart.com/' 
+        }
+    ]);
+
+    await request.post('https://todo.qacart.com/api/v1/tasks', {
+        data:{
+            isCompleted: false,
+            item: "New test - item 2"
+        },
+        headers: {
+            Authorization: `Bearer: ${access_token}`
+        } 
+    })
+
+    await page.goto('https://todo.qacart.com/todo')
+})
