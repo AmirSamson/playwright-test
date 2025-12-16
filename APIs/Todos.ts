15456232056f709fa5ad255c2ce9f269b70d6e07
Amir Samson <amirhsamson93@gmail.com>
2025-12-16 16:32:00 +0330

    added a new Todo API

--- APIs/Todos.ts
+++ APIs/Todos.ts
@@ -13,9 +13,24 @@ export default class TodoAPI{
             Authorization: `Bearer ${user.getaccess_token()}`,
         },
     });
-    console.log('Add Todo status:', response.status());
-    const respBody = await response.json();
-    console.log('Add Todo response body:', respBody);
-    return response;
+        console.log('Add Todo status:', response.status());
+        const respBody = await response.json();
+        console.log('Add Todo response body:', respBody);
+        return response;
+    };
+
+    async addAnotherTodo(request:APIRequestContext, user:User){
+        const response = await request.post('http://todo.qacart.com/api/v1/tasks', {
+            data:{
+                isCompleted: false,
+                item: "new todo / Samson"
+            },
+            headers:{
+                Authorization: `Bearer ${user.getaccess_token()}`
+            },
+        });
+        const respBody = await response.json();
+        console.log('Add Todo response body:', respBody);
+        return response;
     };
 };
